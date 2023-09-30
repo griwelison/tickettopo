cc242a6761418cf097853ccd2002dfe9a2171f08
griwelison <72676195+griwelison@users.noreply.github.com>
2023-09-30 11:47:35 -0400

    Update 20200904070004-create-default-users.ts

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -6,8 +6,8 @@ module.exports = {
       "Users",
       [
         {
-          name: "Press-Ticket",
-          email: "admin@pressticket.com.br",
+          name: "Suporte",
+          email: "sac@topotecnologia.com",
           passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
           profile: "admin",
           tokenVersion: 0,
